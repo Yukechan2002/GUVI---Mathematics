91edc2a95e6c6192d96c79d2f352309261b8ed6a
Yukesh Chandran <167736831+Yukechan2002@users.noreply.github.com>
2024-05-03 09:28:52 +0530

    Create 45. Kabali is a brave warrior who with his group of young ninjas moves from one place to another to fight against his opponents.js
    Kabali is a brave warrior who with his group of young ninjas moves from one place to another to fight against his opponents. Before Fighting he just calculates one thing, the difference between his ninja number and the opponent's ninja number. From this difference he decides whether to fight or not. Kabali's ninja number is never greater than his opponent.
    Input
    The input contains two numbers in every line. These two numbers in each line denotes the number ninjas in Kabali's clan and his opponent's clan . print the absolute difference of number of ninjas between Kabali's clan and his opponent's clan. Each output should be in seperate line.
    Sample Testcase :
    INPUT
    100 200
    OUTPUT
    100

---
+++ 45. Kabali is a brave warrior who with his group of young ninjas moves from one place to another to fight against his opponents.js
@@ -0,0 +1,27 @@
+// Getting input via STDIN
+const readline = require("readline");
+
+const inp = readline.createInterface({
+  input: process.stdin
+});
+
+const userInput = [];
+
+inp.on("line", (data) => {
+  userInput.push(data);
+});
+
+inp.on("close", () => {
+  //start-here
+  //Your code goes here … replace the below line with your code logic 
+
+var input = userInput[0].split(" ");
+input.sort();
+var n1 = input[0];
+var n2 = input[1];
+var output =0;
+
+output = n2-n1;
+console.log(output);
+  //end-here
+});
