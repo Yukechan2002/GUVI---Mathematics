5101d6184857d0f422b1e336efbec2c7d81cbbbc
Yukesh Chandran <167736831+Yukechan2002@users.noreply.github.com>
2024-05-03 09:40:02 +0530

    Create 51. Given a number N, print 'yes' if it is a multiple of 13 else print 'no'.js
    Sample Testcase :
    INPUT
    26
    OUTPUT
    yes

---
+++ 51. Given a number N, print 'yes' if it is a multiple of 13 else print 'no'.js
@@ -0,0 +1,25 @@
+// Getting input via STDIN
+const readline = require("readline");
+
+const inp = readline.createInterface({
+  input: process.stdin
+});
+
+const userInput = [];
+
+inp.on("line", (data) => {
+  userInput.push(data);
+});
+
+inp.on("close", () => {
+  //start-here
+  //Your code goes here … replace the below line with your code logic 
+
+var input = parseInt(userInput[0]);
+if(input % 13 === 0){
+    console.log("yes");
+}
+else
+console.log("no");
+  //end-here
+});
