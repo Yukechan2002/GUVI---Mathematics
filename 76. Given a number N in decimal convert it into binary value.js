b97a508071506c16698858819457a0d36bbc5b80
Yukesh Chandran <167736831+Yukechan2002@users.noreply.github.com>
2024-05-09 15:18:42 +0530

    Create 76. Given a number N in decimal convert it into binary value.js
    Input Size : N <= 100000
    Sample Testcase :
    INPUT
    5
    OUTPUT
    101

---
+++ 76. Given a number N in decimal convert it into binary value.js
@@ -0,0 +1,22 @@
+// Getting input via STDIN
+const readline = require("readline");
+
+const inp = readline.createInterface({
+  input: process.stdin
+});
+
+const userInput = [];
+
+inp.on("line", (data) => {
+  userInput.push(data);
+});
+
+inp.on("close", () => {
+  //start-here
+  //Your code goes here … replace the below line with your code logic 
+
+var N = parseInt(userInput[0]);
+console.log(N.toString(2));
+
+  //end-here
+});
