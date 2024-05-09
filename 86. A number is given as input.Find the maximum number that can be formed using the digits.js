2a4882c2c8bc5e47dadbada610a173c541a7333a
Yukesh Chandran <167736831+Yukechan2002@users.noreply.github.com>
2024-05-09 15:22:36 +0530

    Create 86. A number is given as input.Find the maximum number that can be formed using the digits.js
    Input Size : N <= 10000000
    Sample Testcase :
    INPUT
    4123
    OUTPUT
    4321

---
+++ 86. A number is given as input.Find the maximum number that can be formed using the digits.js
@@ -0,0 +1,23 @@
+// Getting input via STDIN
+const readline = require("readline");
+
+const inp = readline.createInterface({
+  input: process.stdin
+});
+
+const userInput = [];
+
+inp.on("line", (data) => {
+  userInput.push(data);
+});
+
+inp.on("close", () => {
+  //start-here
+  //Your code goes here … replace the below line with your code logic 
+
+var input =  userInput[0].split('');
+input.sort();
+console.log(input.reverse().join(''));
+
+  //end-here
+});
